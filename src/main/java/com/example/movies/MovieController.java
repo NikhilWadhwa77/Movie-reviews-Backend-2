19ba8598d4f78f8eb61dab6fcc291b652f9dc940
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2024-02-06 21:43:35 +0530

    Update MovieController.java

--- src/main/java/com/example/movies/MovieController.java
+++ src/main/java/com/example/movies/MovieController.java
@@ -4,10 +4,6 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,7 +11,7 @@ import java.util.Optional;
 
 import static org.springframework.http.HttpStatus.OK;
 
-@CrossOrigin(origins = "https://movie-reviews-frontend-zeta.vercel.app/")
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
